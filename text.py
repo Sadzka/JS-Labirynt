4bbc7327d748b17fa9836992bfdb7473c892f5e8
Sadzka <sirsadzka@gmail.com>
2020-04-25 13:59:54 +0200

    Button divided into 2 classes

--- text.py
+++ text.py
@@ -1,27 +1,32 @@
 from sfml import sf
 from widget import Widget
 from widget import font
-from text import Text
 
-class Button(Text):
+class Text(Widget):
     def __init__(self, text, x=0, y=0):
-        super().__init__(text, x, y)
+        super().__init__(x, y)
         
         self.text = sf.Text(text)
         self.text.font = font
         self.text.character_size = 20
         
+    def setCharacterSize(size):
+        self.text.character_size = size
+        
+    def getCharacterSize(size) -> int:
+        return self.text.character_size
     
+    def setText(string):
+        self.text.string = string
+        
+    def getText(string):
+        return self.text.string
+        
     def draw(self, window):
         window.draw(self.text)
         
     def selfupdate(self):
         self.text.position = self.position
-    
+        
     def handleEvent(self, event, window):
-        mousepos = sf.Mouse.get_position(window)
-        if event == sf.Event.MOUSE_MOVED:
-            if self.text.global_bounds.contains(mousepos):
-                self.text.color = sf.Color(0, 255, 0)
-            else:
-                self.text.color = sf.Color(255, 255, 255)
+        return
